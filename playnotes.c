651eac9088b7078355bae6e09cc9304e92194cde
Andrew Chen <3952722+andrewchenk@users.noreply.github.com>
2014-04-08 19:36:42 -0700

    counting stars

--- playnotes.c
+++ playnotes.c
@@ -1,3 +1,4 @@
+//useless dance code
 void dance(int beat, int power)
 {
 	ClearTimer(T1);
@@ -8,16 +9,20 @@ void dance(int beat, int power)
 	}
 }
 
+//no dance
 void n(int note, int time)
 {
 	PlayTone(note,time);
 }
 
+//dance forward for 50 ms
 void p(int note, int time)
 {
 	PlayTone(note, time);
 	dance(50, 50);
 }
+
+//dance bakward for 50 ms
 void l(int note, int time)
 {
 	PlayTone(note, time);
@@ -26,7 +31,9 @@ void l(int note, int time)
 
 task main()
 {
-	//defining int tone values for all of the notes
+	//defining int tone values for all of tee notes
+
+	int lowB = 247;
 	int C = 262;
 	int Csharp = 277;
 	int Eb = 311;
@@ -41,52 +48,83 @@ task main()
 	int highC = 523;
 	int highCsharp = 554;
 
-	int h = 30; //half note
-	int w = 60; //whole note
+	int e = 30; //eigth note
+	int q = 60; //quarter note
+	int h = 120; //half note
 
 	//switch case to different tunes
-	int routine = 1;
-	switch(routine)
+	while(true)
 	{
-	case 1:
-		if(nNxtButtonPressed == 1)
+		int routine = 1;
+		switch(routine)
 		{
-			routine=2;
+		case 1:
+			if(nNxtButtonPressed == 1)
+			{
+				routine = 2;
+			}
+			else {
+				//random tune
+				p(A, q);
+				l(Asharp, e);
+				p(B, e);
+				l(Fsharp, e);
+				p(highCsharp, e);
+				l(B, e);
+				p(Fsharp, q);
+				l(A, e);
+			}
+			break;
+		case 2:
+			if(nNxtButtonPressed == 1)
+			{
+				routine = 3;
+			}
+			else {
+				//twinkle twinkle little star
+				n(C, e);
+				n(C, e);
+				n(G, e);
+				n(G, e);
+				n(A, e);
+				n(A, e);
+				n(G, q);
+				n(F, e);
+				n(F, e);
+				n(E, e);
+				n(E, e);
+				n(D, e);
+				n(D, e);
+				n(C, q);
+			}
+			break;
+		case 3:
+			if (nNxtButtonPressed == 1)
+			{
+				routine = 1;
+			}
+			else {
+				//counting stars
+				n(E, q);
+				n(F, q);
+				n(A, q);
+				n(E, q);
+				n(E, e);
+				n(F, e);
+				n(E, e);
+				n(D, e);
+				n(F, h);
+				n(E, e);
+				n(E, e);
+				n(E, e);
+				n(G, e);
+				n(F, e);
+				n(E, e);
+				n(D, e);
+				n(F, q);
+				n(lowB, q);
+				n(D, q);
+			}
 		}
-		else {
-			//random tune
-			p(A, w);
-			l(Asharp, h);
-			p(B, h);
-			l(Fsharp, h);
-			p(highCsharp, h);
-			l(B, h);
-			p(Fsharp, w);
-			l(A, h);
-		}
-		break;
-	case 2:
-		if(nNxtButtonPressed == 1)
-		{
-			routine = 3;
-		}
-		else {
-			//twinkle twinkle little star
-			n(C, h);
-			n(C, h);
-			n(G, h);
-			n(G, h);
-			n(A, h);
-			n(A, h);
-			n(G, w);
-			n(F, h);
-			n(F, h);
-			n(E, h);
-			n(E, h);
-			n(D, h);
-			n(D, h);
-			n(C, w);
-		}
-		break;
 	}
 }
