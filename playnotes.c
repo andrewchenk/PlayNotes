26eeb5cfacd8d3e165a1d60dfe4e8f7519f433b6
Andrew Chen <3952722+andrewchenk@users.noreply.github.com>
2014-04-11 17:33:05 -0700

    shopping trip

--- playnotes.c
+++ playnotes.c
@@ -1,130 +1,58 @@
-//useless dance code
-void dance(int beat, int power)
+//standard function
+void n(int note)
 {
-	ClearTimer(T1);
-	while(time1[T1]<beat)
-	{
-		motor[motorA] = power;
-		motor[motorB] = power;
-	}
+	wait1Msec(300);
+	PlayTone(note, 30);
+	wait1Msec(300);
 }
-
-//no dance
-void n(int note, int time)
+void q(int note)
 {
-	PlayTone(note,time);
-}
-
-//dance forward for 50 ms
-void p(int note, int time)
-{
-	PlayTone(note, time);
-	dance(50, 50);
-}
-
-//dance bakward for 50 ms
-void l(int note, int time)
-{
-	PlayTone(note, time);
-	dance(50, -50);
+	wait1Msec(300);
+	PlayTone(note, 60);
+	wait1Msec(300);
 }
 
 task main()
 {
-	//defining int tone values for all of tee notes
+	//defining int tone values for all of the notes
 
 	int lowB = 247;
 	int C = 262;
 	int Csharp = 277;
-	int Eb = 311;
 	int D = 294;
+	int Dsharp = 311;
 	int E = 330;
 	int F = 349;
 	int Fsharp = 370;
 	int G = 392;
-	int Asharp = 466;
+	int Gsharp;
 	int A = 440;
+	int Asharp = 466;
 	int B = 494;
-	int highC = 523;
-	int highCsharp = 554;
+	int C2 = 523;
 
-	int e = 30; //eigth note
-	int q = 60; //quarter note
-	int h = 120; //half note
-
-	//switch case to different tunes
-	while(true)
-	{
-		int routine = 1;
-		switch(routine)
-		{
-		case 1:
-			if(nNxtButtonPressed == 1)
-			{
-				routine = 2;
-			}
-			else {
-				//random tune
-				p(A, q);
-				l(Asharp, e);
-				p(B, e);
-				l(Fsharp, e);
-				p(highCsharp, e);
-				l(B, e);
-				p(Fsharp, q);
-				l(A, e);
-			}
-			break;
-		case 2:
-			if(nNxtButtonPressed == 1)
-			{
-				routine = 3;
-			}
-			else {
-				//twinkle twinkle little star
-				n(C, e);
-				n(C, e);
-				n(G, e);
-				n(G, e);
-				n(A, e);
-				n(A, e);
-				n(G, q);
-				n(F, e);
-				n(F, e);
-				n(E, e);
-				n(E, e);
-				n(D, e);
-				n(D, e);
-				n(C, q);
-			}
-			break;
-		case 3:
-			if (nNxtButtonPressed == 1)
-			{
-				routine = 1;
-			}
-			else {
-				//counting stars
-				n(E, q);
-				n(F, q);
-				n(A, q);
-				n(E, q);
-				n(E, e);
-				n(F, e);
-				n(E, e);
-				n(D, e);
-				n(F, h);
-				n(E, e);
-				n(E, e);
-				n(E, e);
-				n(G, e);
-				n(F, e);
-				n(E, e);
-				n(D, e);
-				n(F, q);
-				n(lowB, q);
-				n(D, q);
-			}
-		}
-	}
+	n(C);
+	n(C);
+	n(G);
+	n(G);
+	n(A);
+	n(A);
+	q(G);
+	n(F);
+	n(F);
+	n(E);
+	n(E);
+	n(D);
+	n(D);
+	n(C);
+	n(G);
+	n(A);
+	n(C2);
+	n(A);
+	n(G);
+	n(G);
+	n(A);
+	n(G);
+	n(F);
+	n(A);
 }
