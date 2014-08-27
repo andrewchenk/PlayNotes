ec1df9b2949b3804753a905ef37b64f8da4de0dc
Andrew Chen <3952722+andrewchenk@users.noreply.github.com>
2014-08-26 20:42:57 -0700

    folder change

---
+++ RobotC/countingstars.c
@@ -0,0 +1,31 @@
+#include "playnotes.c"
+//Counting Stars by One Republic
+task main()
+{
+	while(true)
+	{
+		wait1Msec(300);
+		q(E);
+		q(Fsharp);
+		q(A);
+		q(Fsharp);
+		n(E);
+		n(E);
+		n(Fsharp);
+		n(E);
+		n(D);
+		q(Fsharp);
+		wait1Msec(300);
+		n(E);
+		n(E);
+		n(Fsharp);
+		n(G);
+		n(Fsharp);
+		n(E);
+		n(D);
+		q(Fsharp);
+		q(lowB);
+		q(D);
+		wait1Msec(300);
+	}
+}
---
+++ RobotC/test.c
@@ -0,0 +1,22 @@
+#include "playnotes.c"
+
+task main()
+{
+	while(true)
+	{
+		n(C);
+		n(C);
+		n(G);
+		n(G);
+		n(A);
+		n(A);
+		q(G);
+		n(F);
+		n(F);
+		n(E);
+		n(E);
+		n(D);
+		n(D);
+		n(C);
+	}
+}
